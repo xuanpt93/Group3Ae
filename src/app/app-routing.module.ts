96bca234f4e21b305e1195e87a79193bc7925ad9
BonTx <bonpham172@gmail.com>
2021-11-17 11:10:11 +0700

    update edit code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoomdetailComponent } from './roomdetail/roomdetail.component';
+import { RoomdetailModule } from './roomdetail/roomdetail.module';
+import { GalleryModule } from './gallery/gallery.module';
+import { GalleryComponent } from './gallery/gallery.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'roomdetail', component: RoomdetailComponent},
+  {path:'galley', component:  GalleryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/roomdetail/roomdetail.component.ts
+++ src/app/roomdetail/roomdetail.component.ts
@@ -6,7 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./roomdetail.component.css']
 })
 export class RoomdetailComponent implements OnInit {
-
+roomdetails: any[] = [
+  {
+    id: 1,
+    text1: 'Guest House',
+   conten: 'Mdodern Guest Rooms',
+   conten1: 'Avoids pleasure itself, because pleasure, but because those who do not know how to pursue pleasure rationally encounter',
+   infor: '3 Bed',
+   infor1: '2 Baths',
+   infor2: '72 m',
+   price: '180.00'
+  },
+  {
+    id: 2,
+    text1: 'Guest House',
+   conten: 'Conference Room',
+   conten1: 'Great explorer of the truth, the master- builder of human happiness one rejects, dislikes avoids pleasure because',
+   infor: '3 Bed',
+   infor1: '2 Baths',
+   infor2: '72 m',
+   price: '250.00'
+  },
+  {
+    id: 3,
+    text1: 'Guest House',
+   conten: 'Deluxe Couple Room',
+   conten1: 'Avoids pleasure itself, because pleasure, but because those who do not know how to pursue pleasure rationally encounter',
+   infor: '3 Bed',
+   infor1: '2 Baths',
+   infor2: '72 m',
+   price: '180.00'
+  }
+]
   constructor() { }
 
   ngOnInit(): void {
