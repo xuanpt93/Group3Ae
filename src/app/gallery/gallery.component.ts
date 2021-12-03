aafbf794cd4c589f5ef127f4487f3f5a534b894e
BonTx <bonpham172@gmail.com>
2021-12-03 10:23:03 +0700

    update Gallery

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-gallery',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
+  [x: string]: any;
 gallays: any[] =[
   {
     id: 1,
@@ -32,29 +34,60 @@ gallays: any[] =[
     txt: 'Conference',
   }
 ]
-squares: any[]=[
- { id: 1,
-name: 'Luxuery Hotel'},
- { id: 1,
-name: 'Luxuery Hotel'},
- { id: 1,
-name: 'Luxuery Hotel'},
- { id: 1,
-name: 'Luxuery Hotel'},
- { id: 1,
-name: 'Luxuery Hotel'},
- { id: 1,
-name: 'Luxuery Hotel'},
- { id: 1,
-name: 'Luxuery Hotel'},
- { id: 1,
-name: 'Luxuery Hotel'},
- { id: 1,
-name: 'Luxuery Hotel'},
+data: any[]=[
+ { id: 1, name: 'Luxuery Hotel',gimg1: 'assets\img\img\download.jpeg',},
+ { id: 2, name: 'Luxuery Hotel'},
+ { id: 3, name: 'Luxuery Hotel'},
+ { id: 4, name: 'Luxuery Hotel'},
+ { id: 5, name: 'Luxuery Hotel'},
+ { id: 6, name: 'Luxuery Hotel'},
+ { id: 7, name: 'Luxuery Hotel'},
+ { id: 8, name: 'Luxuery Hotel'},
+ { id: 9, name: 'Luxuery Hotel 1',gimg1: 'assets\img\img\download.jpeg'},
+ { id: 10,name: 'Luxuery Hotel 1'},
+ { id: 11, name: 'Luxuery Hotel 1'},
+ { id: 12, name: 'Luxuery Hotel'},
+ { id: 13, name: 'Luxuery Hotel'},
+ { id: 14, name: 'Luxuery Hotel'},
+ { id: 15, name: 'Luxuery Hotel'},
+ { id: 16, name: 'Luxuery Hotel'},
+ { id: 17, name: 'Luxuery Hotel'},
+ { id: 18, name: 'Luxuery Hotel'},
+ { id: 19, name: 'Luxuery Hotel 2'},
+ { id: 20,name: 'Luxuery Hotel 3'},
+ { id: 21, name: 'Luxuery Hotel 2'},
+ { id: 22, name: 'Luxuery Hotel'},
+ { id: 23, name: 'Luxuery Hotel'},
+ { id: 24, name: 'Luxuery Hotel'},
+ { id: 25, name: 'Luxuery Hotel 5'},
+ { id: 26, name: 'Luxuery Hotel'},
+ { id: 27, name: 'Luxuery Hotel'},
 ]
-  constructor() { }
+pageData: any[] =[];
+pageSize: number = 9;
+totalpage: number = 0;
+currentPage: number = 1;
+pages: any[] = [];
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadPaginate(1);
   }
 
+  loadPaginate(page: number){
+    this.totalpage = Math.ceil(this.data.length / this.pageSize);
+    this.pages = new Array(this.totalpage);
+    let start = (page - 1)* this.pageSize;
+    let end = start + this.pageSize;
+    this.pageData = this.data.slice(start, end);
+    this.currentPage = page;
+  }
+  prepage():void{
+    this.currentPage--;
+    this.loadPaginate(this.currentPage);
+  }
+  nextpage():void{
+    this.currentPage++;
+    this.loadPaginate(this.currentPage);
+  }
 }
