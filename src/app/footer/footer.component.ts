c21b621d7710080b93e24bf012005fb509c28530
Dũng Việt <dungviet@MacBook-Pro-cua-Dung.local>
2021-12-15 21:41:33 +0700

    edit home

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,7 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  footer_top:any[]=[
+    {
+      id: 1,
+      img: 'assets/img/img-home/01.png'
+    },
+    {
+      id: 2,
+      img: 'assets/img/img-home/02.png'
+    },
+    {
+      id: 3,
+      img: 'assets/img/img-home/03.png'
+    },
+    {
+      id: 4,
+      img: 'assets/img/img-home/04.png'
+    },
+    {
+      id: 5,
+      img: 'assets/img/img-home/05.png'
+    },
+    {
+      id: 6,
+      img: 'assets/img/img-home/06.png'
+    }
+]
   constructor() { }
 
   ngOnInit(): void {
